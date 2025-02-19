456fee92af1e7d33709bb6ea3fd752f9f3b8ee3f
Anton Sokolov <anton.s@codexoptimus.pro>
2025-02-19 20:51:41 +0700

    v0.6.0

--- lib/servactory/rubocop/version.rb
+++ lib/servactory/rubocop/version.rb
@@ -4,8 +4,8 @@ module Servactory
   module Rubocop
     module VERSION
       MAJOR = 0
-      MINOR = 5
-      PATCH = 1
+      MINOR = 6
+      PATCH = 0
       PRE = nil
 
       STRING = [MAJOR, MINOR, PATCH, PRE].compact.join(".")
